dc585e3e90157a0e203ab1f2113943250efcd8a8
Maique Douglas <rmaique762@gmail.com>
2024-05-21 13:50:41 -0300

    Atualização das imagens

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -14,21 +14,7 @@ function Banner(){
                         Tenha ajuda psicológica baseada em evidências<br/>
                         para as suas dificuldades e transtornos emocionais.
                     </p>
-                    <button>Agende sua consulta</button>
-                    <div className='icons'>
-                        <div className='whatsapp'>
-                            <img src="https://img.icons8.com/ios/50/whatsapp--v1.png" alt="whatsapp--v1" width="30" height="30"/>
-                            <span>(61) 9 9844 - 3434</span>
-                        </div>
-                        <div className='insta'>
-                            <img width="30" height="30" src="https://img.icons8.com/ios/50/instagram-new--v1.png" alt="instagram-new--v1"/>
-                            <span>@helengama.neuropsi</span>
-                        </div>
-                        <div className='e-mail'>
-                            <img width="30" height="30" src="https://img.icons8.com/ios/50/new-post--v1.png" alt="new-post--v1"/>
-                            <span>neuropsicologa@helengama.com</span>
-                        </div>
-                    </div>                              
+                    <button>Agende sua consulta</button>                
                 </div>         
             </div>
         </div>
--- src/Components/Specialty/Specialty.jsx
+++ src/Components/Specialty/Specialty.jsx
@@ -1,14 +1,14 @@
 import './Specialty.css';
-import conjugal from "../../assets/conjugal.jpeg";
-import familiar from "../../assets/familiar.jpeg";
-import neuro from "../../assets/neuro.jpeg";
-import avaliação from "../../assets/avaliação.jpeg";
+import conjugal from "../../assets/conjugal.png";
+import familiar from "../../assets/familiar.png";
+import neuro from "../../assets/neuro.png";
+import avaliação from "../../assets/avaliação.png";
 
 
 function Specialty(){
     return(
         <div className='container-specialty'>
-            <h1>Especialidades</h1>
+            
             <div className='specialty'>
                <div className='conjugal'>
                <img src={conjugal} alt='terapia conjugal'/>
