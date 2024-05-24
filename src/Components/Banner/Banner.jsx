2314716cdff8304f29336c9ac88bcec72c5d2582
Maique Douglas <rmaique762@gmail.com>
2024-05-23 23:18:11 -0300

    melhoria no código

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,12 +1,9 @@
 import './Banner.css';
-import img2 from "../../assets/banner-2.png";
+import img from "../../assets/banner-1.png";
 
 function Banner(){
     return (
         <div className="container-banner">
-            <div className='img2'>
-                <img src={img2} alt='imagem2'/> 
-            </div>
             <div className='banner'> 
                 <div className='text'>
                     <h1>Uma Psicóloga de Família</h1>
@@ -14,7 +11,10 @@ function Banner(){
                         Tenha ajuda psicológica baseada em evidências<br/>
                         para as suas dificuldades e transtornos emocionais.
                     </h1>
-                    <button>Agende sua consulta</button>                
+                    <button>Agende sua consulta</button>  
+                    <div className='img'>
+                <img src={img} alt='imagem'/> 
+            </div>              
                 </div>         
             </div>
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,10 +5,11 @@ import About from "../../Components/About/About";
 import Specialty from "../../Components/Specialty/Specialty";
 import Whatsapp from "../../Components/Whastapp/Whatsapp";
 import HowItWorks from "../../Components/HowItWorks/HowItWorks";
+import './Home.css';
 
 function Home(){
     return(
-        <div>
+        <div className="container">
             <Header/>
             <Banner/>
             <HowItWorks/>
